1956838b61216b89215419ef4d7f6b7930e7c8ec
Bearskie <Bearskie@users.noreply.github.com>
2017-11-15 18:01:46 +0800

    Failed attempt

--- countryname_extract.py
+++ countryname_extract.py
@@ -9,7 +9,7 @@ r = praw.Reddit(client_id = 'YOUR_CLIENT_ID_HERE', client_secret = 'YOUR_CLIENT_
 print("Logging on...")
 
 namefile = open('countryname.txt','r')
-alias_dict = {}
+#alias_dict = {}
 count_dict = {}
 country_list = []
 
@@ -19,18 +19,50 @@ if ws.cell(row=1,column=3).value != "id":
 	print ("ID cannot be found")
 	sys.exit()
 	
+alias_dict = ({},{},{})
+special = ['@','%','<','>']
+len_sp = len(special)-1
+len_al = len(alias_dict)-1
+
 for line in namefile:
 	pair = line.rstrip().split('=',1)
 	if len(pair) == 1:
 		pair.insert(0,pair[-1].split('/')[0])
 	for i in pair[-1].split('/'):
-		alias_dict[i] = pair[0]
+		if i[0] == '+':
+			alias_dict[0][i[1:]] = pair[0]
+		elif i[0] == '-':
+			alias_dict[2][i[1:]] = pair[0]
+		else:
+			alias_dict[1][i] = pair[0]
+		#alias_dict[i] = pair[0]
 	country_list.append(pair[0])
 
 def dropchar(text):
+	for c in special:
+		text = text.replace(c,'')
 	return re.sub('[^a-zA-Z ]', ' ', text)
+	
+def match_country(match):
+	if match in parsed:
+		if parsed.count(match) > 1:
+			print (logtext + ' ' + alias + ' x' + str(parsed.count(match)))
+		else:
+			print (logtext + ' ' + alias)
+		count_dict[alias_dict[alias]] += multiplier
+		parsed = parsed.replace(match,'')
 
-j = 2
+def find_country():
+	lpad = '' if '<' in alias[0:len_sp] else ' '
+	rpad = ' ' if '>' in alias[0:len_sp] else ''
+	content = dropchar(alias)
+	match_country(lpad + content + rpad)
+	
+	if '%' in alias[0:len_sp]:
+		content = '.'.join(e for e in alias if e.isalpha())
+		match_country(lpad + content + rpad)	
+						
+j = 10000#2
 for row in range(j,len(ws['C'])):
 	post = r.submission(id=ws.cell(row=j,column=3).value)
 #for post in r.subreddit('polandball').new(limit=None):
@@ -51,58 +83,31 @@ for row in range(j,len(ws['C'])):
 			multiplier = 1
 			logtext = "     [User]"
 		
-		#first pass for special '' alias
-		for alias in alias_dict:
-			if alias[0] == "'" and alias[-1] == "'":
-				match = ' ' + dropchar(alias[1:-1]) + ' '
-				if match in parsed:
-					if parsed.count(match) > 1:
-						print (logtext + ' ' + alias + ' x' + str(parsed.count(match)))
-					else:
-						print (logtext + ' ' + alias)
-					count_dict[alias_dict[alias]] += multiplier
-					parsed = parsed.replace(match,'')
-					
-		#lowercase for rest
+		#first pass @
+		for priority in range(0,len_al):
+			for alias in alias_dict[priority]:	
+				if '@' in alias[0:len_sp]:
+					find_country()
+		#lower pass
 		parsed = parsed.lower()
-		for alias in alias_dict:
-			if alias[0] != "'" or alias[-1] != "'":
-				match = ' ' + dropchar(alias).lower() #left padding - word can extend right but not left
-				if match in parsed:
-					if parsed.count(match) > 1:
-						print (logtext + ' ' + alias + ' x' + str(parsed.count(match)))
-					else:
-						print (logtext + ' ' + alias)
-					count_dict[alias_dict[alias]] += multiplier
-					parsed = parsed.replace(match,'')
-	
+		for priority in range(0,len(alias_dict)-1):
+			for alias in alias_dict[priority]:
+				if '@' not in alias[0:len_sp]:
+					find_country()
 	#title
 	parsed = ' ' + dropchar(post.title) + ' '
 	multiplier = 20
 	logtext = "     [TITLE]"
-	
-	for alias in alias_dict:
-		if alias[0] == "'" and alias[-1] == "'":
-			match = ' ' + dropchar(alias[1:-1]) + ' '
-			if match in parsed:
-				if parsed.count(match) > 1:
-					print (logtext + ' ' + alias + ' x' + str(parsed.count(match)))
-				else:
-					print (logtext + ' ' + alias)
-				count_dict[alias_dict[alias]] += multiplier
-				parsed = parsed.replace(match,'')
-				
+
+	for priority in range(0,len_al):
+		for alias in alias_dict[priority]:	
+			if '@' in alias[0:len_sp]:
+				find_country()
 	parsed = parsed.lower()
-	for alias in alias_dict:
-		if alias[0] != "'" or alias[-1] != "'":
-			match = ' ' + dropchar(alias).lower()
-			if match in parsed:
-				if parsed.count(match) > 1:
-					print (logtext + ' ' + alias + ' x' + str(parsed.count(match)))
-				else:
-					print (logtext + ' ' + alias)
-				count_dict[alias_dict[alias]] += multiplier
-				parsed = parsed.replace(match,'')
+	for priority in range(0,len_al):
+		for alias in alias_dict[priority]:
+			if '@' not in alias[0:len_sp]:
+				find_country()
 				
 	#count stats
 	total = 0.0
